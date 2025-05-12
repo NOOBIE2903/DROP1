12357103926f8c98735f1ea0c0c380f059700fdb
Abhi Jain <156125621+NOOBIE2903@users.noreply.github.com>
2025-05-12 23:45:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const token = "YoAucXpwL-zcctPdsg9c2GO4ANoZywyv";
 
 async function fetchData() {
   try {
-    const [t1, t2, h1, h2, w1, w2, aqi1, aqi2, m1] = await Promise.all([
+    const [t1, t2, h1, h2, w1, w2, aqi1, aqi2] = await Promise.all([
       fetch(`https://blynk.cloud/external/api/get?token=${token}&V0`).then(r => r.text()), // Temperature
       fetch(`https://blynk.cloud/external/api/get?token=${token}&V3`).then(r => r.text()), // Temperature
       fetch(`https://blynk.cloud/external/api/get?token=${token}&V1`).then(r => r.text()), // Humidity
@@ -17,21 +17,18 @@ async function fetchData() {
       fetch(`https://blynk.cloud/external/api/get?token=${token}&V5`).then(r => r.text()), // Water Level
       fetch(`https://blynk.cloud/external/api/get?token=${token}&V6`).then(r => r.text()), // Water Level
       fetch(`https://blynk.cloud/external/api/get?token=${token}&V7`).then(r => r.text()), // Air Quality
-      fetch(`https://blynk.cloud/external/api/get?token=${token}&V8`).then(r => r.text()), // Air Quality
-      fetch(`https://blynk.cloud/external/api/get?token=${token}&V9`).then(r => r.text())  // Motion
+      fetch(`https://blynk.cloud/external/api/get?token=${token}&V8`).then(r => r.text())  // Air Quality
     ]);
 
     const avgTemp = ((parseFloat(t1) + parseFloat(t2)) / 2).toFixed(2);
     const avgHum = ((parseFloat(h1) + parseFloat(h2)) / 2).toFixed(2);
     const avgWaterLevel = ((parseFloat(w1) + parseFloat(w2)) / 2).toFixed(2);
     const avgAqi = Math.round((parseInt(aqi1) + parseInt(aqi2)) / 2);
-    const motionStatus = m1 === "1" ? "Detected" : "Not Detected";
 
     document.getElementById("temp").innerText = avgTemp + " °C";
     document.getElementById("humidity").innerText = avgHum + " %";
     document.getElementById("water-level").innerText = avgWaterLevel + " cm";
     document.getElementById("air-quality").innerText = avgAqi + " ppm";
-    document.getElementById("motion-status").innerText = motionStatus;
 
   } catch (err) {
     console.error("Error fetching data from Blynk:", err);
@@ -39,7 +36,6 @@ async function fetchData() {
     document.getElementById("humidity").innerText = "Error";
     document.getElementById("water-level").innerText = "Error";
     document.getElementById("air-quality").innerText = "Error";
-    document.getElementById("motion-status").innerText = "Error";
   }
 }
 
